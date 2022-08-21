9a954e767c5eef8a50c8c36c67684be31e2ca88f
Sam Deane <sam@elegantchaos.com>
2022-08-21 17:09:37 +0100

    added action tests

--- configure.lua
+++ configure.lua
@@ -1,9 +1,10 @@
-local useLocal = false --export: Use require() to load local scripts if present. Useful during development.
-local logging = false --export: Enable controller debug output.
-local logElements = false --export: Log all discovered elements.
+local useLocal = true --export: Use require() to load local scripts if present. Useful during development.
+local logging = true --export: Enable controller debug output.
+local logElements = true --export: Log all discovered elements.
 
 modulaSettings = { 
     name = "Test Construct",
+    version = "1.0",
     logging = logging, 
     logElements = logElements, 
     useLocal = useLocal,
@@ -11,123 +12,27 @@ modulaSettings = {
         ["samedicorp.modula-test.module"] = {}
     },
     actions = {
-        speeddown = {
-            target = "throttle",
-            loop = "decreaseThrottle"
-        },
-
-        speedup = {
-            target = "throttle",
-            loop = "increaseThrottle"
-        },
-
-        stopengines = {
-            target = "throttle",
-            start = "stopEngines"
-        },
-
         brake = {
-            target = "braking",
-            start = "activateBrake",
-            stop = "deactivateBrake"
+            target = "test",
+            start = "startTest",
+            stop = "stopTest"
         },
 
-        yawleft = {
-            target = "impulse",
-            onoff = "yawLeft",
-        },
 
-        yawright = {
-            target = "impulse",
-            onoff = "yawRight"
-        },
-
-        strafeleft = {
-            target = "impulse",
-            onoff = "strafeLeft"
-        },
-
-        straferight = {
-            target = "impulse",
-            onoff = "strafeRight"
-        },
-
-        right = {
-            target = "impulse",
-            onoff = "rollRight",
-        },
-
-        left = {
-            target = "impulse",
-            onoff = "rollLeft",
-        },
-
-        forward = {
-            target = "impulse",
-            onoff = "pitchDown",
-        },
-
-        backward = {
-            target = "impulse",
-            onoff = "pitchUp",
-        },
-
-        up = {
-            target = "impulse",
-            onoff = "up"
-        },
-
-        down = {
-            target = "impulse",
-            onoff = "down"
-        },
-
-        groundaltitudeup = {
-            target = "stabilisation",
-            loop = "raiseTarget"
-        },
-
-        groundaltitudedown = {
-            target = "stabilisation",
-            loop = "lowerTarget"
-        },
-
-        gear = {
-            target = "parking",
-            stop = "toggleLanding",
-            long = "forceLanding"
-        },
-
-        warp = {
-            target = "warp",
-            start = "activate"
-        },
-        
         option1 = {
-            target = "display",
-            start = "menuPrevious"
+            target = "test",
+            onoff = "startStopTest",
         },
 
         option2 = {
-            target = "display",
-            start = "menuNext"
+            target = "test",
+            loop = "loopTest"
         },
 
-        option3 = {
-            target = "display",
-            start = "menuSelect"
+        gear = {
+            target = "test",
+            stop = "normalPressTest",
+            long = "longPressTest"
         },
-
-        option4 = {
-            target = "display",
-            start = "menuBack"
-        },
-
-        option5 = {
-            target = "bookmarks",
-            stop = "nextWaypoint",
-            long = "storeWaypoint"
-
-        }
     }
 }
--- module.lua
+++ module.lua
@@ -6,15 +6,52 @@
 local Module = { }
 
 function Module:register(modula, parameters)
-    modula:registerForEvents({"onStart", "onStop"}, self)
+    modula:registerForEvents({"onStart", "onStop", "onCommand"}, self)
+    modula:registerService("test", self)
 end
 
 function Module:onStart()
     debug("Test module was started")
+    player.freeze(1)
 end
 
 function Module:onStop()
     debug("Test module was stopped")
 end
+
+function Module:onCommand(command, arguments)
+    if command == "test" then
+        print("Hello from the test module")
+    end
+end
+
+-- ---------------------------------------------------------------------
+-- Example action handlers
+-- ---------------------------------------------------------------------
+
+function Module:startTest()
+    print("test action started")
+end
+
+function Module:stopTest()
+    print("test action stopped")
+end
+
+function Module:startStopTest(action)
+    print("test action %s", action)
+end
+
+function Module:loopTest()
+    print("test looping")
+end
+
+function Module:normalPressTest()
+    print("normal press")
+end
+
+function Module:longPressTest()
+    print("long press")
+end
+
 
 return Module
