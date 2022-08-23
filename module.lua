cf7aa83e157797010f93910bf796119023b4fb6b
Sam Deane <sam@elegantchaos.com>
2022-08-23 20:00:58 +0100

    fixed module

--- module.lua
+++ module.lua
@@ -1,12 +1,13 @@
--- -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+-- -=module.lua-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 --  Created by Samedi on 20/08/2022.
 --  All code (c) 2022, The Samedi Corporation.
--- -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+-- -=module.lua-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 local Module = { }
 
 function Module:register(modula, parameters)
     self.called = {}
+    self.elapsed = 1
 
     modula:registerForEvents({"onStart", "onStop", "onCommand", "onFlush", "onUpdate", "onSlowUpdate", "onFastUpdate"}, self)
     modula:registerService("test", self)
@@ -17,13 +18,10 @@ end
 -- ---------------------------------------------------------------------
 
 function Module:onStart()
-    debug("Test module was started")
-
-    self.elapsed = 1
+    debugf("Test module was started")
 
     local panels = self.modula:getService("panels")
     if panels then
-        print("found panels service")
         self.panel = panels:addPanel("test", "Test")
         self.widgets = self.panel:addWidgets({
             foo = { value = "This is some text" },
@@ -34,16 +32,28 @@ function Module:onStart()
         self.widgets.bar:update(1.23)
     end
 
+    local screen = self.modula:getService("screen")
+    if screen then
+        local window = self._window
+        window.width = window.width or "4vw"
+        window.height = window.height or "12vw"
+        window.position = window.position or "absolute"
+        window.content = ""
+        window.name = "airspeed"
+    
+        screen:addWindow(window)
+    end
+
     player.freeze(1)
 end
 
 function Module:onStop()
-    debug("Test module was stopped")
+    debugf("Test module was stopped")
 end
 
 function Module:onCommand(command, arguments)
     if command == "test" then
-        print("Hello from the test module")
+        printf("Hello from the test module")
     end
 end
 
@@ -71,36 +81,36 @@ end
 -- ---------------------------------------------------------------------
 
 function Module:startTest()
-    print("test action started")
+    printf("test action started")
 end
 
 function Module:stopTest()
-    print("test action stopped")
+    printf("test action stopped")
 end
 
 function Module:startStopTest(action)
-    print("test action %s", action)
+    printf("test action %s", action)
 end
 
 function Module:loopTest()
-    print("test looping")
+    printf("test looping")
 end
 
 function Module:normalPressTest()
-    print("normal press")
+    printf("normal press")
 end
 
 function Module:longPressTest()
-    print("long press")
+    printf("long press")
 end
 
 -- ---------------------------------------------------------------------
 
 function Module:reportCalled(handler)
-    if not self.called[handler] then
-        self.called[handler] = true
-        print("%s called", handler)
-    end
+    -- if not self.called[handler] then
+    --     self.called[handler] = true
+    --     printf("%s called", handler)
+    -- end
 end
 
 return Module
